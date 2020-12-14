f98b05c38bca20439a62d582400c58312cb68bab
robertrobotics <34439559+robertrobotics@users.noreply.github.com>
2020-12-14 22:20:40 +0100

    Initialization of basic Flutter's app

---
+++ android/app/src/main/kotlin/com/example/flutter_plantapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_plantapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plantapp/constants.dart';
+import 'package:flutter_plantapp/screens/home/components/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primaryColor: kPrimaryColor,
+          textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: HomeScreen(),
+    );
+  }
+}
